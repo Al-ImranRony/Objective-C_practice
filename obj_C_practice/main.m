e7355b22b6851d6d8bfb05529b67d14e57a6a9a7
kamal <kamal.mpoffice@gmail.com>
2021-06-10 14:20:15 +0600

    Initial Commit

---
+++ obj_C_practice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  obj_C_practice
+//
+//  Created by sz ashik on 10/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
